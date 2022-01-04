e3bf0832f935099e2b7b852452d2549a883c7658
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-01-04 12:40:52 -0300

    config firebase

--- nlw-06-brunobarbosa/src/App.tsx
+++ nlw-06-brunobarbosa/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Salve!</h1>
+      <Button text="botao 1"/>
+      <Button />
+      <Button />
+      <Button />
     </div>
   );
 }
---
+++ nlw-06-brunobarbosa/src/services/firebase.ts
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBY378oZLDgm-uK6QYVsXwZHcr8JHEvZm4",
+  authDomain: "letmeask-brunobarbossa.firebaseapp.com",
+  databaseURL: "https://letmeask-brunobarbossa-default-rtdb.firebaseio.com",
+  projectId: "letmeask-brunobarbossa",
+  storageBucket: "letmeask-brunobarbossa.appspot.com",
+  messagingSenderId: "646885070978",
+  appId: "1:646885070978:web:57ec8aa8b2a85f0d2cfc9b"
+};
+
+firebase.initializeApp(firebaseConfig)

0db090081fec37ce7f9c1d552b7eedfce57aaf2f
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-01-05 00:31:49 -0300

    estilização da home

--- nlw-06-brunobarbosa/src/pages/Home.tsx
+++ nlw-06-brunobarbosa/src/pages/Home.tsx
@@ -2,17 +2,18 @@ import illustrationImg from '../assets/illustration.svg'
 import logoImg from '../assets/logo.svg';
 import googleIconImg from '../assets/google-icon.svg';
 
+import '../styles/auth.scss'
 
 export default function Home() {
   return (
-    <div>
+    <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div>
+        <div className="main-content">
           <img src={logoImg} alt="Imagem do logo" />
           <button>
             <img src={googleIconImg} alt="Logo da Google" />

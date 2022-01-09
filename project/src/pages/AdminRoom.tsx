fd532ae122bac9cb404bb01b8b7a386f0536bffd
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-01-09 01:46:35 -0300

    encerrar sala

--- project/src/pages/AdminRoom.tsx
+++ project/src/pages/AdminRoom.tsx
@@ -4,7 +4,7 @@ import RoomCode from '../components/RoomCode'
 
 import '../styles/room.scss'
 
-import { useParams } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 import { FormEvent, useState } from 'react';
 import { useAuth } from '../hooks/useAuth'
 import { database } from '../services/firebase'
@@ -20,12 +20,27 @@ type RoomParams = {
 
 
 export function AdminRoom() {
+  const history = useHistory();
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const [newQuestion, setNewQuestion] = useState('');
   
   const {title, questions} = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Deseja excluir esta pergunta?')) {
+      const questionRef = await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+    }
+  }
+
+  async function handleEndRoom() {
+    database.ref(`rooms/${roomId}`).update({
+      endAt: new Date(),
+    })
+
+    history.push('/')
+  }
 
   return (
     <div id="page-room">
@@ -34,7 +49,7 @@ export function AdminRoom() {
           <img src={logoImg} alt="Logo do sistema" />
           <div>
             <RoomCode code={roomId} />
-            <Button isOutlined>Encerrar sala</Button>
+            <Button isOutlined onClick={handleEndRoom}>Encerrar sala</Button>
           </div>
         </div>
       </header>
@@ -52,7 +67,9 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}>
-                <button>
+                <button 
+                type='button'
+                onClick={() => handleDeleteQuestion(question.id)}>
                   <img src={deleteImg} alt="Deletar pergunta" />
                 </button>
                   </Question>
--- project/src/pages/Home.tsx
+++ project/src/pages/Home.tsx
@@ -38,6 +38,10 @@ export default function Home() {
       return alert('Sala inexistente!');
     }
 
+    if(roomRef.val().endAt) {
+      return alert('Esta sala já foi fechada!')
+    }
+
     history.push(`/rooms/${roomCode}`)
   }
 
